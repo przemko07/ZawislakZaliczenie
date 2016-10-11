4ed377fa9ac2da915c4328c906f76984d774eb84
przemko <przemyslaw.sojka@evatronix.com>
2016-10-11 16:04:46 +0200

    graph

---
+++ WpfFrontend/View/GraphV.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfFrontend.View
+{
+    /// <summary>
+    /// Interaction logic for GraphV.xaml
+    /// </summary>
+    public partial class GraphV : UserControl
+    {
+        public GraphV()
+        {
+            InitializeComponent();
+        }
+    }
+}
